d7e859dcb412f1eaf25d99b69024ed8b1ce629dc
Vishad Saxena <vishad1710@gmail.com>
2022-06-03 21:40:00 +0530

    updated funtranslation links

--- app.js
+++ app.js
@@ -6,8 +6,32 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function gerTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with the server, try again after sometime");
+}
+
 function clickEventHandler(){
-    outputDiv.innerText = "ajsajsajsajas " + txtInput.value;
+    //outputDiv.innerText = "ajsajsajsajas " + txtInput.value;
+
+    var inputText = txtInput.value; //Taking input
+
+    //calling server
+    fetch(gerTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
